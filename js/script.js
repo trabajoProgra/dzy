491eb10be8e46028835a3f8e1f5dbbaecfece049
MrEdwar <129194779+MrEdwar@users.noreply.github.com>
2023-04-25 12:11:07 -0400

    se borro la alerta del script

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         document.getElementById('menu-superior').outerHTML
             = this.responseText;
-        alert(4);
     }
 }
 xhttp.open('GET', 'menu.html', true);
